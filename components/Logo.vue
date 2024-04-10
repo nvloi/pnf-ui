294b46cfd4ab260d1c628d80295bb5a18f90a7d4
Loi Nguyen <loi.nguyen@icariohealth.com>
2024-04-10 14:36:26 +0700

    init commit

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-img width="151" height="55" fill="none" contain :src="`/logo.png`"></v-img>
+</template>
+
+<style scoped>
+.fill-theme-color {
+  fill: #1976d2;
+}
+.fill-light-color {
+  fill: #fff;
+}
+.fill-dark-color {
+  fill: #000;
+}
+</style>
+<script setup lang="ts"></script>
